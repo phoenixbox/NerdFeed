ba8f352ac1cc778ead25e239b8d1705e55959575
phoenixbox <shane@quickleft.com>
2013-12-01 14:40:42 -0700

    Initial Commit

---
+++ NerdFeed/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NerdFeed
+//
+//  Created by Shane Rogers on 12/1/13.
+//  Copyright (c) 2013 Shane Rogers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
